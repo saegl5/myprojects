af95ece884ff08e67cb2320184b6106b8ad1a13d
Ed Silkworth <ed@questabox.com>
2021-01-01 14:23:10 -0500

    renamed files

--- Codes/pygame-animation-bouncing-part-3.py
+++ Codes/pygame-animation-bouncing-part-3.py
@@ -16,6 +16,12 @@ clock = pygame.time.Clock() # define "clock"
 # offset = 0 # initialize offset earlier
 offset = 100 # start below top edge
 change = 50 # initialize increment early
+# rect_x = 50 # initialize starting x position of the rectangle
+# rect_y = 50 # initialize starting y position of the rectangle
+# rect_change_x = 1
+# rect_change_y = 5 
+# # lower number moves rectangle slower, higher moves it faster
+# # # negative number changes direction
 
 pygame.display.set_caption("QUESTABOX's Cool Animation") # title, or choose your own
  
@@ -37,6 +43,13 @@ while not done: # meaning while true, loop keeps window open
     # if 0+offset == 0: # if rectangle at top edge
     if 0+offset == 100: # if rectangle below top edge
         change *= -1 # change the increment's sign back
+    # pygame.draw.rect(screen, BLACK, (rect_x, rect_y, 60, 50), width=0)
+    # rect_x += rect_change_x
+    # rect_y += rect_change_y
+    # if rect_y > 450 or rect_y < 0:
+    #     rect_change_y = rect_change_y * -1 # bounce off bottom or top edges
+    # if rect_x > 640 or rect_x < 0:
+    #     rect_change_x = rect_change_x * -1 # bounce off left or right edges
     # ----------------
     pygame.display.flip() # update the screen
     # clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through While loop each second)
