ea3eeef1df1aeafd7fbbced5d00b261dd94233a7
Ed Silkworth <ed@questabox.com>
2021-01-06 15:15:49 -0500

    renamed file

--- Codes/pygame-animation-snowfall-part-2.py
+++ Codes/pygame-animation-snowfall-part-2.py
@@ -1,5 +1,4 @@
 import pygame # import the Pygame library of functions
-import random # import module
 pygame.init() # initialize the game engine
  
 LIGHTGRAY = (211, 211, 211) # example
@@ -14,16 +13,11 @@ size = (700, 500) # (width, height) in pixels
 screen = pygame.display.set_mode(size) # set screen size
 done = False # define "done"
 clock = pygame.time.Clock() # define "clock"
-snow_list = [] # define a list
+snowflakes = [] # define a list
 
 pygame.display.set_caption("QUESTABOX's Cool Animation") # title, or choose your own
 
-for i in range(50): # snow flake count
-    x = random.randrange(0, 700)
-    y = random.randrange(0, 500)
-    coordinates = (x, y) # by default, each coordinate is a tuple
-    L = list(coordinates) # convert each coordinate to a list
-    snow_list.append(L) # create a list of random coordinates
+snowflakes.extend([(10, 10), (500, 10), (100, 100), (380, 100), (5, 250), (300, 250), (500, 250), (10, 400), (200, 400), (500, 400)]) # list of center points
 
 while not done: # meaning while true, loop keeps window open
     for event in pygame.event.get(): # check for user input when open window
@@ -31,16 +25,8 @@ while not done: # meaning while true, loop keeps window open
             done = True # change "done" to exit While loop on next loop, loop will not run while false
     screen.fill(BLACK) # clear the screen
     # --- Drawing code
-    for i in range(len(snow_list)):
-        pygame.draw.circle(screen, LIGHTGRAY, snow_list[i], radius=5)
-        snow_list[i][1] += 1 # snowfall speed, increasing y by 1 pixel for each coordinate
-        if snow_list[i][1] > 505: # snow flake has moved off the bottom of the screen
-            # Reset it just above the top
-            y = random.randrange(-50, -5)
-            snow_list[i][1] = y
-            # Give it a new x position
-            x = random.randrange(0, 700)
-            snow_list[i][0] = x
+    for i in range(0, len(snowflakes)):
+        pygame.draw.circle(screen, LIGHTGRAY, snowflakes[i], radius=3, width=0)
     # ----------------
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through While loop each second)
