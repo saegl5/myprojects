7baf57afc0874678d6210e52a2e35e82e65c3e38
Ed Silkworth <ed@questabox.com>
2021-01-11 21:48:10 -0500

    changed variable names

--- Codes/pygame-interaction-part-1.py
+++ Codes/pygame-interaction-part-1.py
@@ -13,19 +13,20 @@ size = (700, 500) # (width, height) in pixels
 screen = pygame.display.set_mode(size) # set screen size
 done = False # define "done"
 clock = pygame.time.Clock() # define "clock"
-# defines offsets in function call
+# offsets are defined by mouse/trackpad position
+# no increments are defined
 pygame.mouse.set_visible(False) # hide pointer
 
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, or choose your own
 
 # --- Functions
-def draw_snowman(screen, x, y):
+def draw_snowman(screen, x_offset, y_offset):
     # Draw a circle for the head
-    pygame.draw.ellipse(screen, LIGHTGRAY, [35 + x, 0 + y, 25, 25])
+    pygame.draw.ellipse(screen, LIGHTGRAY, [35 + x_offset, 0 + y_offset, 25, 25])
     # Draw the middle snowman circle
-    pygame.draw.ellipse(screen, LIGHTGRAY, [23 + x, 20 + y, 50, 50])
+    pygame.draw.ellipse(screen, LIGHTGRAY, [23 + x_offset, 20 + y_offset, 50, 50])
     # Draw the bottom snowman circle
-    pygame.draw.ellipse(screen, LIGHTGRAY, [0 + x, 65 + y, 100, 100])
+    pygame.draw.ellipse(screen, LIGHTGRAY, [0 + x_offset, 65 + y_offset, 100, 100])
 # -------------
 
 while not done: # meaning WHILE True, loop keeps window open
@@ -33,14 +34,14 @@ while not done: # meaning WHILE True, loop keeps window open
         if event.type == pygame.QUIT: # user clicked close button
             done = True # change "done" to exit WHILE loop on next loop, loop will not run WHILE False
     # --- Game logic
-    pos = pygame.mouse.get_pos() # for mouse/trackpad, returns tuple (x, y)
-    x = pos[0]
-    y = pos[1]
+    pos = pygame.mouse.get_pos() # for mouse/trackpad, returns tuple (x_offset, y_offset)
+    x_offset = pos[0]
+    y_offset = pos[1]
     # --------------
     screen.fill(BLACK) # clear the screen
     # --- Drawing code
     # draw_snowman(screen, 10, 10) # numbers are offsets
-    draw_snowman(screen, x, y) # rely on mouse/trackpad
+    draw_snowman(screen, x_offset, y_offset) # rely on mouse/trackpad
     # ----------------
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
