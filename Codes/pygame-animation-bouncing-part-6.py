cd867e479508acdaa2212603defd5ef9d1d4b287
Ed Silkworth <ed@questabox.com>
2021-01-12 22:20:08 -0500

    new file

--- Codes/pygame-animation-bouncing-part-6.py
+++ Codes/pygame-animation-bouncing-part-6.py
@@ -1,60 +1,43 @@
-import pygame # import the Pygame library of functions
-pygame.init() # initialize the game engine
+# give student a drawing of multiple shapes, and have them animate it
 
-LIGHTGRAY = (211, 211, 211) # example
-# (Red, Green, Blue) = (0-255, 0-255, 0-255)
-# 0-255 is amount of color
-# the amount of color is additive
-# can also choose your own color
-# parentheses of numbers (e.g., (211, 211, 211)) is called a tuple
-BLACK = (0, 0, 0) # example
+import pygame
+pygame.init()
 
-# size = (700, 500) # (width, height) in pixels
-size = (700, 400) # changed height, so rectangle can bounce around more (could also have changed width or rectangle width or height)
-screen = pygame.display.set_mode(size) # set screen size
-done = False # define "done"
-clock = pygame.time.Clock() # define "clock"
-# offset = 0 # initialize offset earlier
-y_offset = 50 # initialize offset earlier, keep starting position at top edge, was 0
-x_offset = 126 # keep starting position at left edge, was 0, then 70
-# increment = 50 # initialize increment early
-y_increment = 50 # initialize increment early
-x_increment = 126 # initialize increment early, was 70
+LIGHTGRAY = (211, 211, 211)
+BLACK = (0, 0, 0)
 
-pygame.display.set_caption("QUESTABOX's Cool Animation") # title, or choose your own
+# size = (100, 200)
+size = (200, 200) # changed width, so rectangle could bounce around more
+screen = pygame.display.set_mode(size)
+done = False
+clock = pygame.time.Clock()
+y_offset = 0 # <- STEP 2b, changed to "y_offset"
+x_offset = 0 # <- STEP 2 (horizontal movement)
+y_increment = 20 # <- STEP 4, changed to "y_increment"
+x_increment = 10 # <- STEP 4
+
+pygame.display.set_caption("STUDENT's Cool Animation")
  
-while not done: # meaning WHILE True, loop keeps window open
-    for event in pygame.event.get(): # check for user input when open window
-        if event.type == pygame.QUIT: # user clicked close button
-            done = True # change "done" to exit WHILE loop on next loop, loop will not run WHILE False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
     screen.fill(LIGHTGRAY) # clear the screen
     # --- Drawing code
-    # offset = 0 # initialize offset
-    # while offset <= 450: # loop until offset = 450 (inclusive)
-    #     pygame.draw.rect(screen, BLACK, (0, 0+offset, 70, 50), width=0) # added one offset to one y-coordinate
-    #     offset += 50 # offset = offset + 50
-    # pygame.draw.rect(screen, BLACK, (0, 0+offset, 70, 50), width=0) # untab
-    # pygame.draw.rect(screen, BLACK, (0, 0+y_offset, 70, 50), width=0) # untab
-    pygame.draw.rect(screen, BLACK, (0+x_offset, 0+y_offset, 70, 50), width=0) # untab
-    # offset += 50 # untab
-    # offset += increment # allow the increment to change
-    y_offset += y_increment # allow the increment to change
-    x_offset -= x_increment # allow the increment to change, changed to decrement
-    # if 0+offset + 50 == size[1]: # if rectangle at bottom edge
-    # if 0+y_offset + 50 == size[1]: # if rectangle at bottom edge
-    if 0+y_offset + 50 == size[1] or 0+y_offset == 0: # if rectangle at bottom or top edge
-        # increment *= -1 # increment = increment*-1, that is change the increment's sign
-        y_increment *= -1 # y_increment = y_increment*-1, that is change the increment's sign
-    # elif 0+offset == 0: # else if rectangle at top edge
-    # elif 0+y_offset == 0: # else if rectangle at top edge
-        # increment *= -1 # change the increment's sign back
-        # y_increment *= -1 # change the increment's sign back
-    if 0+x_offset + 70 == size[0] or 0+x_offset == 0: # if rectangle at right or left edge
-        x_increment *= -1 # x_increment = x_increment*-1, that is change the increment's sign
-    # elif 0+x_offset == 0: # else if rectangle at left edge
-    #     x_increment *= -1 # change the increment's sign back
+    # offset = 0 <- STEP 2a
+    # while offset <= 80: <- STEP 0
+    pygame.draw.rect(screen, BLACK, (0+x_offset, 0+y_offset, 10, 20), width=0) # untab <- "offset" is STEP 1 (could also offset x-coordinate), "offset" became "y_offset"
+    y_offset += y_increment # untab, "increment" is STEP 3, "increment" became "y_increment"
+    x_offset += x_increment # <- STEP 3
+    if 0+y_offset + 20 == size[1]: # <- STEP 5a
+        y_increment *= -1
+    elif 0+y_offset == 0: # <- STEP 5b
+        y_increment *= -1
+    if 0+x_offset + 10 == size[0]: # <- STEP 5a
+        x_increment *= -1
+    elif 0+x_offset == 0: # <- STEP 5b
+        x_increment *= -1
     # ----------------
-    pygame.display.flip() # update the screen
-    # clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
-    clock.tick(1) # so can see rectangle moving, was 10, then 30
-pygame.quit() # formality
+    pygame.display.flip()
+    clock.tick(10)
+pygame.quit()
