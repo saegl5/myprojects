52f93880cc8b48cfbb7bb0678031e6e5b0ac1796
Ed Silkworth <ed@questabox.com>
2021-01-23 00:02:35 -0500

    renamed file and merged it with another

--- Codes/pygame-animation-bouncing-part-6.py
+++ Codes/pygame-animation-bouncing-part-6.py
@@ -10,13 +10,13 @@ LIGHTGRAY = (211, 211, 211) # example
 BLACK = (0, 0, 0) # example
 
 # size = (700, 500) # (width, height) in pixels
-size = (700, 400) # changed height, so rectangle can bounce around more (could also have changed width or rectangle width or height)
+size = (700, 400) # changed height, so rectangle can bounce around more (could also have changed width)
 screen = pygame.display.set_mode(size) # set screen size
 done = False # define "done"
 clock = pygame.time.Clock() # define "clock"
 # offset = 0 # initialize offset earlier
 y_offset = 50 # initialize offset earlier, keep starting position at top edge, was 0
-x_offset = 126 # keep starting position at left edge, was 0, then 70
+x_offset = 90 # keep starting position at left edge, was 0, then 70
 # increment = 50 # initialize increment early
 y_increment = 50 # initialize increment early
 x_increment = 126 # initialize increment early, was 70
@@ -42,17 +42,21 @@ while not done: # meaning WHILE True, loop keeps window open
     x_offset -= x_increment # allow the increment to change, changed to decrement
     # if 0+offset + 50 == size[1]: # if rectangle at bottom edge
     # if 0+y_offset + 50 == size[1]: # if rectangle at bottom edge
-    if 0+y_offset + 50 == size[1] or 0+y_offset == 0: # if rectangle at bottom or top edge
+    # if 0+y_offset + 50 == size[1]: # if rectangle at bottom edge
         # increment *= -1 # increment = increment*-1, that is change the increment's sign
-        y_increment *= -1 # y_increment = y_increment*-1, that is change the increment's sign
+        # y_increment *= -1 # y_increment = y_increment*-1, that is change the increment's sign
     # elif 0+offset == 0: # else if rectangle at top edge
     # elif 0+y_offset == 0: # else if rectangle at top edge
         # increment *= -1 # change the increment's sign back
         # y_increment *= -1 # change the increment's sign back
-    if 0+x_offset + 70 == size[0] or 0+x_offset == 0: # if rectangle at right or left edge
-        x_increment *= -1 # x_increment = x_increment*-1, that is change the increment's sign
+    if 0+y_offset + 50 == size[1] or 0+y_offset == 0: # if rectangle at bottom or top edge
+        y_increment *= -1 # y_increment = y_increment*-1, that is change the increment's sign
+    # if 0+x_offset + 70 == size[0]: # if rectangle at right edge
+        # x_increment *= -1 # x_increment = x_increment*-1, that is change the increment's sign
     # elif 0+x_offset == 0: # else if rectangle at left edge
     #     x_increment *= -1 # change the increment's sign back
+    if 0+x_offset + 70 >= size[0] or 0+x_offset <= 0: # if rectangle at right or left edge
+        x_increment *= -1 # x_increment = x_increment*-1, that is change the increment's sign
     # ----------------
     pygame.display.flip() # update the screen
     # clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
