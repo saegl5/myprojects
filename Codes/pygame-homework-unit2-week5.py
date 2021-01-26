30dd4c473e35cb76e29d93dfa90a31ed5a0318c5
Ed Silkworth <ed@questabox.com>
2021-01-26 14:28:20 -0500

    added homework template

---
+++ Codes/pygame-homework-unit2-week5.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+pygame.init()
+
+LIGHTGRAY = (211, 211, 211)
+BLACK = (0, 0, 0)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+done = False
+clock = pygame.time.Clock()
+snowflakes = []
+i = int() # optional
+
+pygame.display.set_caption("QUESTABOX's Cool Animation")
+
+for i in range(0, 200):
+    x = random.randrange(0, size[0]+1)
+    y = random.randrange(0, size[1]+1)
+    snowflakes.append((x, y))
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    screen.fill(BLACK)
+    for i in range(0, len(snowflakes)):
+        pygame.draw.circle(screen, LIGHTGRAY, snowflakes[i], radius=3, width=0)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
