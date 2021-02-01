b29717703c6095eeb68930a506788d18a8b365b8
Ed Silkworth <ed@questabox.com>
2021-02-01 15:29:27 -0500

    using blueprints and integrating ideas from unit iii

--- Codes/pygame-canvas-reinforced.py
+++ Codes/pygame-canvas-reinforced.py
@@ -1,14 +1,14 @@
 import pygame # import the Pygame library of functions
 pygame.init() # initialize the game engine
  
-LIGHTGRAY = (211, 211, 211) # example
+BLUE = (0, 0, 255) # example
 # (Red, Green, Blue) = (0-255, 0-255, 0-255)
 # 0-255 is amount of color
 # the amount of color is additive
 # can also choose your own color
 # parentheses of numbers (e.g., (211, 211, 211)) is called a tuple
  
-size = (700, 500) # (width, height) in pixels
+size = (704, 512) # (width, height) in pixels
 screen = pygame.display.set_mode(size) # set screen size
 done = False # define "done"
 clock = pygame.time.Clock() # define "clock"
@@ -19,7 +19,7 @@ while not done: # meaning WHILE True, loop keeps window open
     for event in pygame.event.get(): # check for user input when open window
         if event.type == pygame.QUIT: # user clicked close button
             done = True # change "done" to exit WHILE loop on next loop, loop will not run WHILE False
-    screen.fill(LIGHTGRAY) # clear the screen
+    screen.fill(BLUE) # clear the screen
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
 pygame.quit() # formality
