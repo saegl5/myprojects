8b78187bcea51b267748e32dfdeca74346ba9a9a
Ed Silkworth <ed@questabox.com>
2021-02-01 23:51:48 -0500

    using blueprints and integrating ideas of unit iii

--- Codes/pygame-interaction-part-1.py
+++ Codes/pygame-interaction-part-1.py
@@ -1,28 +1,28 @@
 import pygame # import the Pygame library of functions
 pygame.init() # initialize the game engine
- 
-CYAN = pygame.Color("cyan") # example, alternative style
+
+BLUE = pygame.Color("blue") # example, alternative style
 # (Red, Green, Blue) = (0-255, 0-255, 0-255)
 # 0-255 is amount of color
 # the amount of color is additive
 # can also choose your own color
 # parentheses of numbers (e.g., (211, 211, 211)) is called a tuple
-BLACK = (0, 0, 0)
+WHITE = pygame.Color("white")
  
-size = (700, 500) # (width, height) in pixels
+size = (704, 512) # (width, height) in pixels
 screen = pygame.display.set_mode(size) # set screen size
 done = False # define "done"
 clock = pygame.time.Clock() # define "clock"
 # offsets are defined by mouse/trackpad position
 # no increments are defined
-pygame.mouse.set_visible(False) # hide pointer
 
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, or choose your own
 
 # --- Functions
-def draw_snowman(screen, x_offset, y_offset):
-    # Draw an ellipse
-    pygame.draw.ellipse(screen, CYAN, [0+x_offset, 0+y_offset, 25, 25])
+def draw_circle(screen, x_offset, y_offset):
+    # Draw a circle
+    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=50, width=1)
+    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=1, width=1)
 # -------------
 
 while not done: # meaning WHILE True, loop keeps window open
@@ -34,10 +34,10 @@ while not done: # meaning WHILE True, loop keeps window open
     x_offset = pos[0]
     y_offset = pos[1]
     # --------------
-    screen.fill(BLACK) # clear the screen
+    screen.fill(BLUE) # clear the screen
     # --- Drawing code
-    # draw_snowman(screen, 10, 10) # numbers are offsets
-    draw_snowman(screen, x_offset, y_offset) # rely on mouse/trackpad
+    # draw_circle(screen, 10, 10) # numbers are offsets
+    draw_circle(screen, x_offset, y_offset) # rely on mouse/trackpad
     # ----------------
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
--- Codes/pygame-interaction-part-2.py
+++ Codes/pygame-interaction-part-2.py
@@ -1,15 +1,15 @@
 import pygame # import the Pygame library of functions
 pygame.init() # initialize the game engine
  
-CYAN = pygame.Color("cyan") # example, alternative style
+BLUE = pygame.Color("blue") # example, alternative style
 # (Red, Green, Blue) = (0-255, 0-255, 0-255)
 # 0-255 is amount of color
 # the amount of color is additive
 # can also choose your own color
 # parentheses of numbers (e.g., (211, 211, 211)) is called a tuple
-BLACK = (0, 0, 0)
+WHITE = pygame.Color("white")
  
-size = (700, 500) # (width, height) in pixels
+size = (704, 512) # (width, height) in pixels
 screen = pygame.display.set_mode(size) # set screen size
 done = False # define "done"
 clock = pygame.time.Clock() # define "clock"
@@ -21,13 +21,10 @@ y_increment = 0
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, or choose your own
 
 # --- Functions
-def draw_snowman(screen, x_offset, y_offset):
-    # Draw a circle for the head
-    pygame.draw.ellipse(screen, CYAN, [35+x_offset, 0+y_offset, 25, 25])
-    # Draw the middle snowman circle
-    pygame.draw.ellipse(screen, CYAN, [23+x_offset, 20+y_offset, 50, 50])
-    # Draw the bottom snowman circle
-    pygame.draw.ellipse(screen, CYAN, [0+x_offset, 65+y_offset, 100, 100])
+def draw_circle(screen, x_offset, y_offset):
+    # Draw a circle
+    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=50, width=1)
+    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=1, width=1)
 # -------------
 
 while not done: # meaning WHILE True, loop keeps window open
@@ -37,13 +34,13 @@ while not done: # meaning WHILE True, loop keeps window open
         # --- Keyboard events
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT: # note "event.key"
-                x_increment = -5
+                x_increment = -7
             elif event.key == pygame.K_RIGHT:
-                x_increment = 5
+                x_increment = 7
             elif event.key == pygame.K_UP: # recall that y increases going downward
-                y_increment = -5 # note "y_increment"
+                y_increment = -7 # note "y_increment"
             elif event.key == pygame.K_DOWN:
-                y_increment = 5
+                y_increment = 7
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_increment = 0
@@ -53,18 +50,18 @@ while not done: # meaning WHILE True, loop keeps window open
     x_offset += x_increment
     y_offset += y_increment
     if 0+x_offset < 0:
-        x_offset = 0 # prevent shape from passing left edge, based on bottom snowman circle
-    elif 0+x_offset + 100 > size[0]:
-        x_offset = size[0] - 100 # from x_offset + 100 = size[0], solved for x_offset
+        x_offset = 0 # prevent center point from passing left edge
+    elif 0+x_offset > size[0]:
+        x_offset = size[0]
     if 0+y_offset < 0:
-        y_offset = 0 # prevent shape from passing top edge, based on circle for the head
-    elif 0+y_offset + 165 > size[1]:
-        y_offset = size[1] - 165 # based on circle for the head, middle snowman circle, and bottom snowman circle, subtracted overlap
+        y_offset = 0 # prevent center point from passing top edge
+    elif 0+y_offset> size[1]:
+        y_offset = size[1]
     # --------------
-    screen.fill(BLACK) # clear the screen
+    screen.fill(BLUE) # clear the screen
     # --- Drawing code
-    # draw_snowman(screen, 10, 10) # numbers are offsets
-    draw_snowman(screen, x_offset, y_offset) # rely on keyboard
+    # draw_circle(screen, 10, 10) # numbers are offsets
+    draw_circle(screen, x_offset, y_offset) # rely on keyboard
     # ----------------
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
