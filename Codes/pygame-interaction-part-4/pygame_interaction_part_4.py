e7d388c49f026a742abbcc2c9e39f9cdee392615
Ed Silkworth <ed@questabox.com>
2021-03-12 22:59:40 -0500

    redid function

--- Codes/pygame-interaction-part-4/pygame_interaction_part_4.py
+++ Codes/pygame-interaction-part-4/pygame_interaction_part_4.py
@@ -18,10 +18,9 @@ y_increment = 0
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, or choose your own
 
 # --- Functions
-def draw_circle(screen, x_offset, y_offset):
+def draw_circle(x_offset, y_offset, radius):
     # Draw a circle
-    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=25, width=1)
-    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=1, width=1)
+    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius, width=1)
 # -------------
 
 while not done: # meaning WHILE True, loop keeps window open
@@ -70,7 +69,8 @@ while not done: # meaning WHILE True, loop keeps window open
     screen.fill(BLUE) # clear the screen
     # --- Drawing code
     # draw_circle(screen, 10, 10) # numbers are offsets
-    draw_circle(screen, x_offset, y_offset) # rely on keyboard
+    draw_circle(x_offset, y_offset, 25) # rely on keyboard
+    draw_circle(x_offset, y_offset, 1)
     # ----------------
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
--- Codes/pygame_interaction_part_1.py
+++ Codes/pygame_interaction_part_1.py
@@ -15,10 +15,9 @@ clock = pygame.time.Clock() # define "clock"
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, or choose your own
 
 # --- Functions
-def draw_circle(screen, x_offset, y_offset):
+def draw_circle(x_offset, y_offset, radius):
     # Draw a circle
-    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=25, width=1)
-    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius=1, width=1)
+    pygame.draw.circle(screen, WHITE, (0+x_offset, 0+y_offset), radius, width=1)
 # -------------
 
 while not done: # meaning WHILE True, loop keeps window open
@@ -35,7 +34,8 @@ while not done: # meaning WHILE True, loop keeps window open
     screen.fill(BLUE) # clear the screen
     # --- Drawing code
     # draw_circle(screen, 10, 10) # numbers are offsets
-    draw_circle(screen, x_offset, y_offset) # rely on mouse/trackpad
+    draw_circle(x_offset, y_offset, 25) # rely on mouse/trackpad
+    draw_circle(x_offset, y_offset, 1)
     # ----------------
     pygame.display.flip() # update the screen
     clock.tick(60) # maximum 60 frames per second (i.e., no more than 60 times through WHILE loop each second)
