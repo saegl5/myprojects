d79d4492a076db3e6f830a1e56cd2765a6e66597
Ed Silkworth <ed@questabox.com>
2021-03-15 00:39:05 -0400

    edited file

---
+++ Codes/Games/pictionary.py
@@ -0,0 +1,39 @@
+import pygame
+pygame.init()
+
+BLUE = pygame.Color("blue")
+WHITE = pygame.Color("white")
+ 
+size = (704, 512)
+screen = pygame.display.set_mode(size)
+done = False
+clock = pygame.time.Clock()
+draw = False # start as "False," so won't draw unless press mouse/trackpad button and move
+
+pygame.display.set_caption("QUESTABOX's 'Pictionary' Game")
+
+def draw_circle(x, y, radius):
+    pygame.draw.circle(screen, WHITE, (x, y), radius, width=1)
+
+screen.fill(BLUE) # placed outside WHILE loop
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            draw = True # replaced drawing code with this
+        elif event.type == pygame.MOUSEBUTTONUP: # new
+            draw = False # new
+        else:
+            None
+    pos = pygame.mouse.get_pos()
+    x_offset = pos[0]-size[0]/2
+    y_offset = pos[1]-size[1]/2
+    if draw == True: # IF mouse/trackpad button pressed
+        draw_circle(size[0]/2+x_offset, size[1]/2+y_offset, 1) # tabbed
+    else:
+        None # do not draw
+    pygame.display.flip()
+    clock.tick(120) # fewer dots
+pygame.quit()
