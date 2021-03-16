41b582d1490e7c2829515e9e33f6567ffe170ac6
Ed Silkworth <ed@questabox.com>
2021-03-16 12:12:43 -0400

    got windows module mixed up with macos one

--- Codes/pygame_test_macos.py
+++ Codes/pygame_test_macos.py
@@ -1,3 +1,2 @@
 import subprocess
-# subprocess.run(("python", "-m", "pygame.examples.aliens"))
-subprocess.run(("py", "-m", "pygame.examples.aliens"))
+subprocess.run(("python", "-m", "pygame.examples.aliens"))
