0aa4b756865b588f3dcd3cb8fa2b713c073ec6b1
Ed Silkworth <ed@questabox.com>
2021-04-26 23:52:39 -0400

    streamlined, corrected, filled shape, changed size

--- Codes/pygame_sprites_part_1.py
+++ Codes/pygame_sprites_part_1.py
@@ -1,12 +1,10 @@
 import pygame # import the pygame module
 import sys # import the sys module
-#### import random # import the random module
 
 pygame.init() # initialize any submodules that require it
 
 BLUE = pygame.Color("blue") # example
 WHITE = pygame.Color("white")
-#### YELLOW = pygame.Color("yellow")
 
 size = (704, 512) # (width, height) in pixels, example
 screen = pygame.display.set_mode(size) # set up display
@@ -15,9 +13,7 @@ x_offset = 0 # reordered
 y_offset = 0
 x_increment = 0
 y_increment = 0
-#### block_list = pygame.sprite.Group() # no longer block_list = []
-sprites = pygame.sprite.Group() # create a list of sprites, no longer sprites = []
-#### score = 0 # initialize score
+sprites = pygame.sprite.Group() # create a list for sprites, no longer sprites = []
 
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, example
 pygame.key.set_repeat(10) # 10 millisecond delay between repeats, optional
@@ -25,26 +21,17 @@ pygame.key.set_repeat(10) # 10 millisecond delay between repeats, optional
 # --- Functions/Classes
 # def draw_rect(display, x, y, W, H):
 #     # Draw a rectangle
-#     pygame.draw.rect(display, screen, WHITE, (x, y, W, H), width=1)
+#     pygame.draw.rect(display, WHITE, (x, y, W, H), width=0)
 class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as sprites, use sentence case to distinguish it from a function
     def __init__(self, COLOR, W, H): # define a constructor (like init() but for classes), class accepts COLOR, width, and height parameters, "__" before and after "init" is a must, requires "self" (like a key to access class)
         super().__init__() # initialize your sprites by calling the constructor of the parent (sprite) class
         size = (W, H) # define size of image, local variable
         self.image = pygame.Surface(size) # makes a blank image using Surface class
-        #### # self.image.fill(WHITE) # background of foreground image
-        #### # self.image.set_colorkey(WHITE) # makes background color transparent
-        pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=1) # draw on image, draw over entire image with (0, 0, W, H)
-        self.rect = self.image.get_rect() # pair image with rectangle object, so we can give the image a position, sprites simply require more work
+        pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=0) # draw on image, draw over entire image with (0, 0, W, H)
+        self.rect = self.image.get_rect() # pair image with rectangle object, so we can give the image a position
 # ---------------------
 
-#### for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50)
-    #### block = Block(YELLOW, 20, 20) # creates a block
-    #### block.rect.x = random.randrange(0, size[0]-20) # position of block image
-    #### block.rect.y = random.randrange(0, size[1]-20)
-    #### block_list.add(block) # no longer append
-    #### sprites.add(block)
-
-player = Rectangle(WHITE, 25, 25) # creates a "player" sprite, which will be your sprite to play with, don't need screen, will instead use it in drawing code, calling class, sans offsets
+player = Rectangle(WHITE, 64, 64) # creates a "player" sprite, which will be your sprite to play with, don't need screen, will instead use it in drawing code, calling class, sans offsets
 sprites.add(player)
 
 while True: # keeps display open
@@ -71,18 +58,6 @@ while True: # keeps display open
     y_offset += y_increment
     player.rect.x = size[0]/2+x_offset # position and offset "player" sprite/image
     player.rect.y = size[1]/2+y_offset
-    #### if player.rect.x < 0:
-        #### player.rect.x = 0 # prevent center point from passing left edge
-    #### elif player.rect.x > size[0]-40: # player block is larger
-        #### player.rect.x = size[0]-40
-    #### if player.rect.y < 0:
-        #### player.rect.y = 0 # prevent center point from passing top edge
-    #### elif player.rect.y > size[1]-40:
-        #### player.rect.y = size[1]-40
-    #### blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True) # list block(s) the player block overlaps, then remove block(s) from block_list and sprites
-    #### for block in blocks_hit_list: # FOR each block in the list
-        #### score +=1 # increment score, resets each time
-        #### print(score) # and print score to console
     # --------------
     screen.fill(BLUE) # clear the display
     # --- Drawing code
