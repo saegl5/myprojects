ca3ef31a372cc94f6b7594f9c6a0025ba527b303
Ed Silkworth <ed@questabox.com>
2021-04-27 23:45:42 -0400

    updated comments

--- Codes/pygame_sprites_part_2.py
+++ Codes/pygame_sprites_part_2.py
@@ -16,7 +16,7 @@ y_offset = 0
 x_increment = 0
 y_increment = 0
 #### block_list = pygame.sprite.Group() # no longer block_list = []
-sprites = pygame.sprite.Group() # create a list for sprites, no longer sprites = []
+sprites = pygame.sprite.Group() # create a list for sprites, no longer sprites = [], Group() is class
 #### score = 0 # initialize score
 
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, example
@@ -26,15 +26,16 @@ pygame.key.set_repeat(10) # 10 millisecond delay between repeats, optional
 # def draw_rect(display, x, y, W, H):
 #     # Draw a rectangle
 #     pygame.draw.rect(display, WHITE, (x, y, W, H), width=0)
-class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as sprites, use sentence case to distinguish it from a function
-    def __init__(self, COLOR, W, H): # define a constructor (like init() but for classes), class accepts COLOR, width, and height parameters, "__" before and after "init" is a must, requires "self" (like a key to access class)
+class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as sprites, use sentence case to distinguish class from a function
+    def __init__(self, COLOR, W, H): # define a constructor, class accepts COLOR, width, and height parameters, must type "__" before and after "init," requires "self"
         super().__init__() # initialize your sprites by calling the constructor of the parent (sprite) class
         size = (W, H) # define size of image, local variable
-        self.image = pygame.Surface(size) # makes a blank image using Surface class
+        self.image = pygame.Surface(size) # creates a blank image using Surface class
         #### # self.image.fill(WHITE) # background of foreground image
         #### # self.image.set_colorkey(WHITE) # makes background color transparent
-        pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=0) # draw on image, draw over entire image with (0, 0, W, H)
-        self.rect = self.image.get_rect() # pair image with rectangle object, so we can give the image a position
+        pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=0) # draw shape on image, draw over entire image with (0, 0, W, H), where (0, 0) is located at image's top-left corner
+        self.rect = self.image.get_rect() # pair image with rectangle object, where (rect.x, rect.y) is located at rectangle object's top-left corner
+        # sprite consists of image and rectangle object
 # ---------------------
 
 #### for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50)
@@ -44,7 +45,7 @@ class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as s
     #### block_list.add(block) # no longer append
     #### sprites.add(block)
 
-player = Rectangle(WHITE, 64, 64) # creates a "player" sprite, which will be your sprite to play with, don't need screen, will instead use it in drawing code, calling class, sans offsets
+player = Rectangle(WHITE, 64, 64) # creates a "player" sprite, which will be your sprite to play with, calling class, don't need screen, will instead use it in drawing code, will use original/starting position and offsets in game logic, specified boundary thickness in class definition
 sprites.add(player)
 
 while True: # keeps display open
@@ -69,7 +70,7 @@ while True: # keeps display open
     # --- Game logic
     x_offset += x_increment
     y_offset += y_increment
-    player.rect.x = size[0]/2+x_offset # position and offset "player" sprite/image
+    player.rect.x = size[0]/2+x_offset # position and offset "player" sprite
     player.rect.y = size[1]/2+y_offset
     #### if player.rect.x < 0:
         #### player.rect.x = 0 # prevent center point from passing left edge
@@ -87,7 +88,8 @@ while True: # keeps display open
     screen.fill(BLUE) # clear the display
     # --- Drawing code
     # draw_rect(screen, size[0]/2+x_offset, size[1]/2+y_offset, 64, 64) # call function, input parameters, and rely on keyboard
-    sprites.draw(screen) # draw sprites on screen
+    # screen.blit(player.image, (player.rect.x, player.rect.y)) # draw sprite on screen
+    sprites.draw(screen) # draw sprites on screen using list
     # ----------------
     pygame.display.flip() # update the display
     clock.tick(60) # maximum 60 frames per second
