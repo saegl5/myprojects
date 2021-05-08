88b3b744617317d0a8e4dc6ed9e3cd563877cc57
Ed Silkworth <ed@questabox.com>
2021-05-08 16:10:51 -0400

    split up modules

--- Codes/pygame_sprites_part_2_timer.py
+++ Codes/pygame_sprites_part_2_timer.py
@@ -7,6 +7,7 @@ pygame.init() # initialize any submodules that require it
 BLUE = pygame.Color("blue") # example
 WHITE = pygame.Color("white")
 YELLOW = pygame.Color("yellow")
+RED = pygame.Color("red")
 
 size = (704, 512) # (width, height) in pixels, example
 screen = pygame.display.set_mode(size) # set up display
@@ -16,10 +17,12 @@ y_offset = 0
 x_increment = 0
 y_increment = 0
 blocks = pygame.sprite.Group() # create a list for "block" sprites, no longer blocks = [], Group() is class
-#### score = 0 # initialize score
 
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, example
 pygame.key.set_repeat(10) # 10 millisecond delay between repeats, optional
+
+timer = 10 # start at 10 seconds
+pygame.time.set_timer(pygame.USEREVENT, 1000) # 1000 milliseconds (i.e., 1 second) between each count down
 
 # --- Functions/Classes
 # def draw_rect(display, x, y, W, H):
@@ -30,18 +33,9 @@ class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as s
         super().__init__() # initialize your sprites by calling the constructor of the parent (sprite) class
         size = (W, H) # define size of image, local variable
         self.image = pygame.Surface(size) # creates a blank image using Surface class
-        #### # self.image.fill(WHITE) # background of foreground image
-        #### # self.image.set_colorkey(WHITE) # makes background color transparent
         pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=0) # draw shape on image, draw over entire image with (0, 0, W, H), where (0, 0) is located at image's top-left corner
         self.rect = self.image.get_rect() # pair image with rectangle object, where (rect.x, rect.y) is located at rectangle object's top-left corner
         # sprite consists of image and rectangle object
-    #### def update(self):
-        #### self.rect.y += 1 # increase y by 1 pixel
-        #### if self.rect.y > size[1]: # IF block has left the canvas, then reset block above canvas (assumes player block has not collided with it)
-            #### self.reset_position()
-    #### def reset_position(self):
-        ##### self.rect.y = random.randrange(-50, -20) # -50 is optional
-        ##### self.rect.x = random.randrange(0, size[0]-20)
 # ---------------------
 
 player = Rectangle(WHITE, 64, 64) # creates a "player" sprite, which will be your sprite to play with, calling class, don't need screen, will instead use it in drawing code, will use original/starting position and offsets in game logic, specified boundary thickness in class definition
@@ -57,6 +51,12 @@ while True: # keeps display open
         if action.type == pygame.QUIT: # user clicked close button
             pygame.quit() # needed if run module through IDLE
             sys.exit() # exit entire process
+        elif action.type == pygame.USEREVENT:
+            timer -= 1
+            if timer == -1: # temporary, so can display 0
+                pygame.time.set_timer(pygame.USEREVENT, 0) # disable timer
+                pygame.quit()
+                sys.exit()
         # --- Mouse/keyboard events
         elif action.type == pygame.KEYDOWN: # "elif" means else if
             if action.key == pygame.K_RIGHT: # note "action.key"
@@ -85,12 +85,6 @@ while True: # keeps display open
     player.rect.x = size[0]/2+x_offset # position and offset "player" sprite
     player.rect.y = size[1]/2+y_offset
     pygame.sprite.spritecollide(player, blocks, True) # remove a "block" sprite, if "player" sprite collides with it
-    #### list block(s) the player block overlaps
-    #### for block in blocks_hit_list: # FOR each block in the list
-        #### score +=1 # increment score, resets each time
-        #### print(score) # and print score to console
-        #### block.reset_position()
-    #### block_list.update() # update each block in block_list
     # --------------
     screen.fill(BLUE) # clear the display
     # --- Drawing code
@@ -98,6 +92,11 @@ while True: # keeps display open
     screen.blit(player.image, (player.rect.x, player.rect.y)) # draw sprite on screen
     # screen.blit(block.image, (block.rect.x, block.rect.y))
     blocks.draw(screen) # draw sprites on screen using list
+    # text_rect = text.get_rect(center = screen.get_rect().center)
+    # screen.blit(text, text_rect)
+    font = pygame.font.Font(None, 100) # faster than SysFont, (filename/object, size in pixels), "None" utilizes default font (i.e., freesansbold.ttf)
+    text = font.render(str(timer), True, RED) # ("string", anti-aliased, color)
+    screen.blit(text, (10, 10))
     # ----------------
     pygame.display.flip() # update the display
     clock.tick(60) # maximum 60 frames per second
