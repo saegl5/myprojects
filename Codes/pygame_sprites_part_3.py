b36851a81b7c3cfcb21b7a750380b0be2a4b3fb3
Ed Silkworth <ed@questabox.com>
2021-05-21 01:21:58 -0400

    moving sprites

--- Codes/pygame_sprites_part_3.py
+++ Codes/pygame_sprites_part_3.py
@@ -32,12 +32,11 @@ class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as s
         size = (W, H) # define size of image, local variable
         self.image = pygame.Surface(size) # creates a blank image using Surface class
         self.image.fill(BLACK) # useful if run module on macOS
-        self.image.set_colorkey(BLACK) # makes background color transparent, useful if run module on Windows
         pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=0) # draw shape on image, draw over entire image with (0, 0, W, H), where (0, 0) is located at image's top-left corner
         self.rect = self.image.get_rect() # pair image with rectangle object, where (rect.x, rect.y) is located at rectangle object's top-left corner
         # sprite consists of image and rectangle object
-    #### def update(self):
-        #### self.rect.y += 1 # increase y by 1 pixel
+    def update(self):
+        self.rect.y += 32 # increase sprites' rect.y by 32 pixels
         #### if self.rect.y > size[1]: # IF block has left the canvas, then reset block above canvas (assumes player block has not collided with it)
             #### self.reset_position()
     #### def reset_position(self):
@@ -91,7 +90,7 @@ while True: # keeps display open
         #### score +=1 # increment score, resets each time
         #### print(score) # and print score to console
         #### block.reset_position()
-    #### block_list.update() # update each block in block_list
+    blocks.update() # move "block" sprites downward, requires timer to move slowly
     # --------------
     screen.fill(BLUE) # clear the display
     # --- Drawing code
