bd84b1d08c703c8177c9c1564152b617dbda5f72
Ed Silkworth <ed@questabox.com>
2021-10-15 18:44:10 -0700

    implemented walls

--- Codes/Pac-Man/pac_man.py
+++ Codes/Pac-Man/pac_man.py
@@ -25,6 +25,7 @@ W = 64 # "player" sprite width reference
 H = 64 # "player" sprite height reference
 blocks = pygame.sprite.Group() # create a list for "block" sprites, no longer blocks = [], Group() is class
 collisions = pygame.sprite.Group()
+walls = pygame.sprite.Group()
 timer = 30 # set timer for 30 seconds
 score = 0 # initialize score
 style = pygame.font.Font(None, 100) # faster than SysFont! (filename/object, font size in pixels), "None" utilizes default font (i.e., freesansbold.ttf)
@@ -47,18 +48,20 @@ pygame.time.set_timer(pygame.USEREVENT, 1000) # count every 1000 milliseconds (i
 
 # --- Functions/Classes
 class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as sprites, use sentence case to distinguish class from a function
-    def __init__(self, sprite_image, W, H): # define a constructor, class accepts COLOR, width, and height parameters, must type "__" before and after "init," requires "self"
+    def __init__(self, sprite_image, x, y, W, H): # define a constructor, class accepts picture, width, height, x-coordinate, and y-coordinate parameters, must type "__" before and after "init," requires "self"
         super().__init__() # initialize your sprites by calling the constructor of the parent (sprite) class
         size = (W, H) # define size of image, local variable
         self.image = pygame.Surface(size) # creates a blank image using Surface class
         self.image.fill(BLACK) # useful if run module on macOS
         # pygame.draw.rect(self.image, COLOR, (0, 0, W, H), width=0) # draw shape on image, draw over entire image with (0, 0, W, H), where (0, 0) is located at image's top-left corner
         self.image.blit(sprite_image, (0, 0))
-        self.image.set_colorkey(BLACK) # windows only
+        # self.image.set_colorkey(BLACK) # windows only
         self.rect = self.image.get_rect() # pair image with rectangle object, where (rect.x, rect.y) is located at rectangle object's top-left corner
         # sprite consists of image and rectangle object
-    def update(self):
-        self.rect.y += 32 # increase sprites' rect.y by 32 pixels
+        self.rect.x = x
+        self.rect.y = y
+    # def update(self):
+        # self.rect.y += 32 # increase sprites' rect.y by 32 pixels
     def turn(self, angle): # calling with sprite, not group
         if count == 1: # in case there is a quick KEYDOWN and KEYUP event
             self.image.blit(player_image_alt, (0, 0))
@@ -68,13 +71,31 @@ class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as s
             self.image = pygame.transform.rotate(player_image, angle)         
         self.image.set_colorkey(BLACK)
 # ---------------------
+# outer walls:
+wall = Rectangle(pygame.Surface([10, 10]), 0, 0, size[0], 10)
+walls.add(wall)
+wall = Rectangle(pygame.Surface([10, 10]), 0, 10, 10, size[1]-10)
+walls.add(wall)
+wall = Rectangle(pygame.Surface([10, 10]), 0, size[1]-10, size[0], 10)
+walls.add(wall)
+wall = Rectangle(pygame.Surface([10, 10]), size[0]-10, 10, 10, size[1]-10)
+walls.add(wall)
 
-player = Rectangle(player_image, W, H) # creates a "player" sprite, which will be your sprite to play with, calling class, don't need screen, will instead use it in drawing code, will use original/starting position and offsets in game logic, specified boundary thickness in class definition
+# inner walls:
+wall = Rectangle(pygame.Surface([10, 10]), 100, 100, size[0]-100-100, 10)
+walls.add(wall)
+wall = Rectangle(pygame.Surface([10, 10]), size[0]/2-10/2, 100, 10, size[1]-100-100-10)
+walls.add(wall)
+wall = Rectangle(pygame.Surface([10, 10]), 100, size[1]-100-10, size[0]-100-100, 10)
+walls.add(wall)
 
-for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50), create and add fifty "block" sprites
-    block = Rectangle(block_image, W/2, H/2) # create a "block" sprite
-    block.rect.x = random.randrange(0, size[0]+1-W/2, W/2) # position "block" sprite, allow it to touch edge but not breach it
-    block.rect.y = random.randrange(0, size[1]+1-H/2, H/2) #  want "block" sprites equally spaced, also mitigates overlap
+player = Rectangle(player_image, size[0]/2+x_offset, size[1]/2+y_offset, W, H) # creates a "player" sprite, which will be your sprite to play with, calling class, don't need screen, will instead use it in drawing code, will no longer use original/starting position and offsets in game logic, used to specify boundary thickness in class definition
+
+# for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50), create and add fifty "block" sprites
+while 50-len(blocks) > 0: # create and add fifty "block" sprites
+    x = random.randrange(0, size[0]+1-W/2, W/2) # position "block" sprite, allow it to touch edge but not breach it
+    y = random.randrange(0, size[1]+1-H/2, H/2) #  want "block" sprites equally spaced, also mitigates overlap
+    block = Rectangle(block_image, x, y, W/2, H/2) # create a "block" sprite
     pygame.sprite.spritecollide(block, blocks, True) # remove any "block" sprite in same position, essentially preventing "block" sprites from taking same position and essentially preventing overlap, you cannot check if sprite is in group or belongs to group since each sprite is unique
     blocks.add(block) # add "block" sprite to list, no longer append
 
@@ -90,25 +111,28 @@ while True: # keeps display open
             elif len(blocks) == 0:
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 you_win_sound.play()
-            else:
+            else: # after one second
                 timer -= 1 # decrement timer
-            # if timer % 5 == 0: # every 5 seconds
-                # blocks.update() # move "block" sprites downward
         # --- Mouse/keyboard events
         elif action.type == pygame.KEYDOWN: # "elif" means else if
-            if timer != 0 and len(blocks) != 0:
-                if action.key == pygame.K_RIGHT: # note "action.key"
-                    x_increment = 5 # "5" is optional
-                    angle = 0
-                elif action.key == pygame.K_UP:
-                    y_increment = -5
-                    angle = 90
-                elif action.key == pygame.K_LEFT:
-                    x_increment = -5
-                    angle = 180
-                elif action.key == pygame.K_DOWN:
-                    y_increment = 5 # note "y_increment," and recall that y increases going downward
-                    angle = 270
+            if action.key == pygame.K_RIGHT: # note "action.key"
+                x_increment = 5 # "5" is optional
+                angle = 0
+                player.turn(angle) # place in IF statement, if using keyboard combination to take screenshots
+                count += 1
+            elif action.key == pygame.K_UP:
+                y_increment = -5
+                angle = 90
+                player.turn(angle)
+                count += 1
+            elif action.key == pygame.K_LEFT:
+                x_increment = -5
+                angle = 180
+                player.turn(angle)
+                count += 1
+            elif action.key == pygame.K_DOWN:
+                y_increment = 5 # note "y_increment," and recall that y increases going downward
+                angle = 270
                 player.turn(angle)
                 count += 1
             else: # without "else," do nothing
@@ -125,18 +149,38 @@ while True: # keeps display open
 
         # -------------------------
     # --- Game logic
-    x_offset += x_increment
-    y_offset += y_increment
-    if size[0]/2+x_offset < 0:
-        x_offset = -size[0]/2 # prevent "player" sprite from breaching left edge, solved for x_offset
-    elif size[0]/2+x_offset + W > size[0]:
-        x_offset = size[0]/2 - W # simplified
-    if size[1]/2+y_offset < 0: # note "if"
-        y_offset = -size[1]/2 # prevent "player" sprite from breaching top edge, solved for y_offset
-    elif size[1]/2+y_offset + H > size[1]:
-        y_offset = size[1]/2 - H # simplified
-    player.rect.x = size[0]/2+x_offset # position and offset "player" sprite
-    player.rect.y = size[1]/2+y_offset
+    # x_offset += x_increment
+    player.rect.x += x_increment # increment directly
+
+    hit = pygame.sprite.spritecollide(player, walls, False) # DON'T remove a "wall" sprite, if "player" sprite hits it
+    # instead...
+    for wall in hit:
+        if x_increment > 0:
+            player.rect.right = wall.rect.left
+        else: # x_increment = 0 not hitting wall
+            player.rect.left = wall.rect.right
+
+    # y_offset += y_increment
+    player.rect.y += y_increment # increment directly, must put here or else goes around wall
+
+    hit = pygame.sprite.spritecollide(player, walls, False)
+    for wall in hit:
+        if y_increment > 0:
+            player.rect.bottom = wall.rect.top
+        else: # y_increment = 0 not hitting wall
+            player.rect.top = wall.rect.bottom
+
+    # if size[0]/2+x_offset < 0:
+    #     x_offset = -size[0]/2 # prevent "player" sprite from breaching left edge, solved for x_offset
+    # elif size[0]/2+x_offset + W > size[0]:
+    #     x_offset = size[0]/2 - W # simplified
+    # if size[1]/2+y_offset < 0: # note "if"
+    #     y_offset = -size[1]/2 # prevent "player" sprite from breaching top edge, solved for y_offset
+    # elif size[1]/2+y_offset + H > size[1]:
+    #     y_offset = size[1]/2 - H # simplified
+
+    # player.rect.x = size[0]/2+x_offset # position and offset "player" sprite <- do earlier
+    # player.rect.y = size[1]/2+y_offset <- do earlier
     # pygame.sprite.spritecollide(player, blocks, True) # remove a "block" sprite, if "player" sprite collides with it
     removed = pygame.sprite.spritecollide(player, blocks, True) # remove a "block" sprite, if "player" sprite collides with it
     collisions.add(removed)
@@ -161,6 +205,7 @@ while True: # keeps display open
     # --- Drawing code
     screen.blit(player.image, (player.rect.x, player.rect.y)) # draw sprite on screen
     blocks.draw(screen) # draw sprites on screen using list
+    walls.draw(screen)
     screen.blit(timer_text, (10, 10)) # copy image of text onto screen at (10, 10)
     screen.blit(score_text, (size[0]-score_text.get_width()-10, 10)) # near top-right corner
     screen.blit(game_over_text, game_over_text.get_rect(center = screen.get_rect().center))
