6f5655571abe4d00547939f5454565888b2670a3
Ed Silkworth <ed@questabox.com>
2021-11-25 14:03:32 -0800

    changed sprite names

--- Codes/Space-Invaders/space_invaders.py
+++ Codes/Space-Invaders/space_invaders.py
@@ -22,14 +22,14 @@ x_offset = 0 # reordered
 # y_offset = 0
 x_increment = 0
 # y_increment = 0
-W = 64 # "player" sprite width reference
-H = 64 # "player" sprite height reference
-blocks = pygame.sprite.Group() # create a list for "block" sprites, no longer blocks = [], Group() is class
+W = 64 # "vehicle" sprite width reference
+H = 64 # "vehicle" sprite height reference
+invaders = pygame.sprite.Group() # create a list for "invader" sprites, no longer invaders = [], Group() is class
 # collisions = pygame.sprite.Group()
 walls = pygame.sprite.Group()
 lasers = pygame.sprite.Group()
 lasers_alt = pygame.sprite.Group()
-players = pygame.sprite.Group()
+vehicles = pygame.sprite.Group()
 timer = 30 # set timer for 30 seconds
 score = 0 # initialize score
 style = pygame.font.Font(None, 100) # faster than SysFont! (filename/object, font size in pixels), "None" utilizes default font (i.e., freesansbold.ttf)
@@ -37,13 +37,13 @@ count = 0 # for lunging picture
 ticks = int() # for saving energy
 game_over_sound = pygame.mixer.Sound('Sounds/game_over.ogg') # Source: https://kenney.nl/assets/voiceover-pack
 you_win_sound = pygame.mixer.Sound('Sounds/you_win.ogg') # Source: https://kenney.nl/assets/voiceover-pack
-player_image = pygame.image.load('Images/ship.png').convert() # Edited from source: https://opengameart.org/content/pixel-space-invaders (changed black to (1, 1, 1), too)
-player_image = pygame.transform.scale(player_image, (W, H))
-# player_image.set_colorkey(BLACK)
-block_image = pygame.image.load('Images/alien.png').convert() # Edited from source: https://opengameart.org/content/alien-sprite-sheet (changed black to (1, 1, 1), too)
-# block_image.set_colorkey(BLACK)
-block_image_alt = pygame.image.load('Images/alien_lunging.png').convert() # my image from alien.png
-# block_image_alt.set_colorkey(BLACK)
+vehicle_image = pygame.image.load('Images/ship.png').convert() # Edited from source: https://opengameart.org/content/pixel-space-invaders (changed black to (1, 1, 1), too)
+vehicle_image = pygame.transform.scale(vehicle_image, (W, H))
+# vehicle_image.set_colorkey(BLACK)
+invader_image = pygame.image.load('Images/alien.png').convert() # Edited from source: https://opengameart.org/content/alien-sprite-sheet (changed black to (1, 1, 1), too)
+# invader_image.set_colorkey(BLACK)
+invader_image_alt = pygame.image.load('Images/alien_lunging.png').convert() # my image from alien.png
+# invader_image_alt.set_colorkey(BLACK)
 
 pygame.display.set_caption("QUESTABOX's Cool Game") # title, example
 pygame.key.set_repeat(10) # 10 millisecond delay between repeats, optional
@@ -67,9 +67,9 @@ class Rectangle(pygame.sprite.Sprite): # make Rectangle class of same class as s
         self.rect.y += px # increase sprites' rect.y by px pixels
     def lunge(self): # calling with sprite, not group
         if count % 2 == 0: # could also have used timer, using count because did same in pac-man
-            self.image.blit(block_image_alt, (0, 0))
+            self.image.blit(invader_image_alt, (0, 0))
         else:
-            self.image.blit(block_image, (0, 0))
+            self.image.blit(invader_image, (0, 0))
 # ---------------------
 
 # outer walls (only left and right):
@@ -81,20 +81,20 @@ wall = Rectangle(size[0]-1+1, 0, 1, size[1])
 walls.add(wall)
 # no inner walls
 
-x = size[0]/2+x_offset # position and offset "player" sprite
+x = size[0]/2+x_offset # position and offset "vehicle" sprite
 y = size[1]-H
-player = Rectangle(x, y, W, H) # creates a "player" sprite, which will be your sprite to play with, calling class, don't need screen, will instead use it in drawing code, will use original/starting position and offsets in game logic, specified boundary thickness in class definition
-player.image.blit(player_image, (0, 0))
-players.add(player)
+vehicle = Rectangle(x, y, W, H) # creates a "vehicle" sprite, which will be your sprite to play with, calling class, don't need screen, will instead use it in drawing code, will use original/starting position and offsets in game logic, specified boundary thickness in class definition
+vehicle.image.blit(vehicle_image, (0, 0))
+vehicles.add(vehicle)
 
-# for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50), create and add fifty "block" sprites
-while 50-len(blocks) > 0: # create and add fifty "block" sprites
-    x = random.randrange(0, size[0]+1-W/2, W/2) # position "block" sprite, allow it to touch edge but not breach it
-    y = random.randrange(0, size[1]+1-H/2-96, H/2) # "-96" leaves space at bottom of canvas and want "block" sprites equally spaced, also mitigates overlap
-    block = Rectangle(x, y, W/2, H/2) # create a "block" sprite
-    block.image.blit(block_image, (0, 0))
-    pygame.sprite.spritecollide(block, blocks, True) # remove any "block" sprite in same position, essentially preventing "block" sprites from taking same position and essentially preventing overlap, you cannot check if sprite is in group or belongs to group since each sprite is unique
-    blocks.add(block) # add "block" sprite to list, no longer append
+# for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50), create and add fifty "invader" sprites
+while 50-len(invaders) > 0: # create and add fifty "invader" sprites
+    x = random.randrange(0, size[0]+1-W/2, W/2) # position "invader" sprite, allow it to touch edge but not breach it
+    y = random.randrange(0, size[1]+1-H/2-96, H/2) # "-96" leaves space at bottom of canvas and want "invader" sprites equally spaced, also mitigates overlap
+    invader = Rectangle(x, y, W/2, H/2) # create a "invader" sprite
+    invader.image.blit(invader_image, (0, 0))
+    pygame.sprite.spritecollide(invader, invaders, True) # remove any "invader" sprite in same position, essentially preventing "invader" sprites from taking same position and essentially preventing overlap, you cannot check if sprite is in group or belongs to group since each sprite is unique
+    invaders.add(invader) # add "invader" sprite to list, no longer append
 
 # we will create "laser" sprites later
 first = True # but only first one
@@ -106,31 +106,31 @@ while True: # keeps display open
             sys.exit() # exit entire process
         elif action.type == pygame.USEREVENT:
             if timer == 0:
-                pygame.time.set_timer(pygame.USEREVENT, 0) # stop timer, "block" sprites stop moving too
+                pygame.time.set_timer(pygame.USEREVENT, 0) # stop timer, "invader" sprites stop moving too
                 game_over_sound.play()
-            elif len(blocks) == 0:
+            elif len(invaders) == 0:
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 you_win_sound.play()
-            elif len(players) == 0:
+            elif len(vehicles) == 0:
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 game_over_sound.play()
             else: # after one second
                 timer -= 1 # decrement timer
                 if timer % 5 == 0: # every 5 seconds
-                    blocks.update(32) # move "block" sprites downward
-                for block in blocks:
-                    block.lunge()
+                    invaders.update(32) # move "invader" sprites downward
+                for invader in invaders:
+                    invader.lunge()
                 count += 1
                 if timer % 7 == 0: # some number not multiple of 5
                     laser = Rectangle(int(), int(), 6, 10) # create "laser" sprite
                     laser.image.fill(RED)
-                    index = 0 # example, more randomized with random.randrange(0, len(blocks))
-                    laser.rect.centerx = blocks.sprites()[index].rect.centerx # align its horizontal center with "invader" sprite's horizontal center
-                    laser.rect.top = blocks.sprites()[index].rect.bottom # align its bottom with "invader" sprite's bottom
+                    index = 0 # example, more randomized with random.randrange(0, len(invaders))
+                    laser.rect.centerx = invaders.sprites()[index].rect.centerx # align its horizontal center with "invader" sprite's horizontal center
+                    laser.rect.top = invaders.sprites()[index].rect.bottom # align its bottom with "invader" sprite's bottom
                     lasers_alt.add(laser)
         # --- Mouse/keyboard events
         elif action.type == pygame.KEYDOWN: # "elif" means else if
-            if timer != 0 and len(blocks) != 0 and len(players) != 0:
+            if timer != 0 and len(invaders) != 0 and len(vehicles) != 0:
                 if action.key == pygame.K_RIGHT: # note "action.key"
                     x_increment = 5 # "5" is optional
                 elif action.key == pygame.K_LEFT:
@@ -138,8 +138,8 @@ while True: # keeps display open
                 elif action.key == pygame.K_SPACE: # fire laser
                     laser = Rectangle(int(), int(), 4, 20) # create "laser" sprite
                     laser.image.fill(CYAN)
-                    laser.rect.centerx = player.rect.centerx # align it with "player" sprite's horizontal center
-                    laser.rect.bottom = player.rect.top + 10 # align its bottom with "player" sprite's top, "+ 10" because update() is called before "laser" sprites are drawn (could probably have also drawn vehicle after lasers)
+                    laser.rect.centerx = vehicle.rect.centerx # align it with "vehicle" sprite's horizontal center
+                    laser.rect.bottom = vehicle.rect.top + 10 # align its bottom with "vehicle" sprite's top, "+ 10" because update() is called before "laser" sprites are drawn (could probably have also drawn vehicle after lasers)
                     if first == True:
                         lasers.add(laser)
                         first = False
@@ -154,35 +154,35 @@ while True: # keeps display open
         # -------------------------
     # --- Game logic
     # x_offset += x_increment
-    player.rect.x += x_increment # offset directly
+    vehicle.rect.x += x_increment # offset directly
 
-    hit = pygame.sprite.spritecollide(player, walls, False) # DON'T remove a "wall" sprite, if "player" sprite hits it, returns a list
+    hit = pygame.sprite.spritecollide(vehicle, walls, False) # DON'T remove a "wall" sprite, if "vehicle" sprite hits it, returns a list
     # instead...
-    for wall in hit: # wall that player hit
+    for wall in hit: # wall that vehicle hit
         if x_increment > 0: # moving rightward
-            player.rect.right = wall.rect.left
+            vehicle.rect.right = wall.rect.left
         else: # moving leftward, x_increment = 0 not hitting wall
-            player.rect.left = wall.rect.right # reverse
+            vehicle.rect.left = wall.rect.right # reverse
 
     # if size[0]/2+x_offset < 0:
-    #     x_offset = -size[0]/2 # prevent "player" sprite from breaching left edge, solved for x_offset
+    #     x_offset = -size[0]/2 # prevent "vehicle" sprite from breaching left edge, solved for x_offset
     # elif size[0]/2+x_offset + W > size[0]:
     #     x_offset = size[0]/2 - W # simplified
-    # player.rect.x = size[0]/2+x_offset # position and offset "player" sprite <- do earlier
-    # player.rect.y = size[1]-H <- do earlier
-    # removed = pygame.sprite.spritecollide(player, blocks, True) # remove a "block" sprite, if "player" sprite collides with it
+    # vehicle.rect.x = size[0]/2+x_offset # position and offset "vehicle" sprite <- do earlier
+    # vehicle.rect.y = size[1]-H <- do earlier
+    # removed = pygame.sprite.spritecollide(vehicle, invaders, True) # remove a "invader" sprite, if "vehicle" sprite collides with it
     for laser in lasers: # "laser" sprite was not created before WHILE loop, for any laser in lasers
-        removed = pygame.sprite.spritecollide(laser, blocks, True) # remove a "block" sprite, if "laser" sprite collides with it
+        removed = pygame.sprite.spritecollide(laser, invaders, True) # remove a "invader" sprite, if "laser" sprite collides with it
         # collisions.add(removed)
-        if removed: # or "for block in removed:"
+        if removed: # or "for invader in removed:"
             lasers.remove(laser) # remove "laser" sprite, too
             score += 1
         elif laser.rect.y < -20:
             lasers.remove(laser) # otherwise, remove "laser" sprite if it exits screen
-    for block in blocks:
-        # touched = pygame.sprite.spritecollide(block, players, True)
-        # players.remove(touched)
-        pygame.sprite.spritecollide(block, players, True)
+    for invader in invaders:
+        # touched = pygame.sprite.spritecollide(invader, vehicles, True)
+        # vehicles.remove(touched)
+        pygame.sprite.spritecollide(invader, vehicles, True)
     if timer != 0:
         # score = len(collisions)
         lasers.update(-10)
@@ -193,22 +193,22 @@ while True: # keeps display open
     score_text = style.render(str(score), True, GREEN)
     game_over_text = style.render(None, True, pygame.Color("black"))
     you_win_text = style.render(None, True, GREEN)
-    if timer == 0 or len(players) == 0:
-        player.image.fill(WHITE)
-        for block in blocks:
-            pygame.draw.rect(block.image, LIGHTGRAY, (0, 0, W/2, H/2), width=0)
+    if timer == 0 or len(vehicles) == 0:
+        vehicle.image.fill(WHITE)
+        for invader in invaders:
+            pygame.draw.rect(invader.image, LIGHTGRAY, (0, 0, W/2, H/2), width=0)
         for laser in lasers:
             pygame.draw.rect(laser.image, LIGHTGRAY, (0, 0, W/2, H/2), width=0)
         screen.fill(GRAY)
         timer_text = style.render(str(timer), True, DARKGRAY)
         score_text = style.render(str(score), True, DARKGRAY)
         game_over_text = style.render("Game Over", True, pygame.Color("black"))
-    if len(blocks) == 0:
+    if len(invaders) == 0:
         you_win_text = style.render("WINNER!", True, GREEN)
     # --- Drawing code
-    screen.blit(player.image, (player.rect.x, player.rect.y)) # draw sprite on screen
+    screen.blit(vehicle.image, (vehicle.rect.x, vehicle.rect.y)) # draw sprite on screen
     walls.draw(screen) # draw sprites on screen using list
-    blocks.draw(screen)
+    invaders.draw(screen)
     lasers.draw(screen)
     lasers_alt.draw(screen)
     screen.blit(timer_text, (10, 10)) # copy image of text onto screen at (10, 10)
