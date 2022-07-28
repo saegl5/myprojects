ecec4c8d8ec370025b2edf98f13ca1c312dfd0da
Ed Silkworth <ed@questabox.com>
2022-07-27 23:42:27 -0700

    added comments

--- src/canvas.py
+++ src/canvas.py
@@ -6,29 +6,33 @@ Manages Screen
 
 import pygame, sys
 
-size = (704, 512)
-screen = pygame.display.set_mode(size)
+size = (704, 512) # (width, height) in pixels, example
+screen = pygame.display.set_mode(size) # set up screen
 
+# function
 def close(): # user clicked close button
     pygame.quit() # needed if run module through IDLE
     sys.exit() # exit entire process
+
+# functions can also be placed in a separate module
+
 
 
 """
 Decorates Screen
 """
 
-pygame.init() # initialize any submodules that require it
+pygame.init() # initialize any submodules (e.g., font and mixer) that require it
 
 BLUE = pygame.Color("blue") # optional color with which to wipe screen
 
 clock = pygame.time.Clock()
 
-def clean():
+def clean(): # wipe the screen
     screen.fill(BLUE)
 # only one line, but clean() is for what fill() is used
 
-def show():
-    pygame.display.flip()
-    clock.tick(60)
+def show(): # showcase your work
+    pygame.display.flip() # literally, update the screen
+    clock.tick(60) # maximum 60 frames per second
 # show() is for what flip() is used
