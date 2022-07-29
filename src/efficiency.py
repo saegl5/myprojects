9705791503994b15c7e72af49b49ea1b05bde62c
Ed Silkworth <ed@questabox.com>
2022-07-29 16:10:42 -0700

    upgraded module

--- src/efficiency.py
+++ src/efficiency.py
@@ -5,12 +5,12 @@ Saves energy (optional)
 import pygame
 import src.canvas as canvas # works, if run from main module
 
-ticks = int()
+ticks = 0
 
 def snapshot(action): # capture current number of clock ticks
     global ticks
-    if action.type == {pygame.KEYUP, pygame.MOUSEBUTTONUP} or action.type is not pygame.MOUSEMOTION:
-        ticks = pygame.time.get_ticks()
+    if action.type == pygame.KEYDOWN or action.type == pygame.MOUSEBUTTONDOWN or action.type == pygame.MOUSEMOTION:
+        ticks = pygame.time.get_ticks() # update ticks
 
 def activate(): # if true, reduce frame rate
     if pygame.time.get_ticks() - ticks > 10000:
