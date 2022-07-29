a64b8efe04e33065a87c7f090d53e3fa3d968e76
Ed Silkworth <ed@questabox.com>
2022-07-29 14:38:13 -0700

    modularized

--- snowfall.py
+++ snowfall.py
@@ -2,52 +2,47 @@
 Snowfall Animation
 """
 
-import pygame, sys, random # import modules
+import pygame, random # import modules
+import src.canvas as canvas
 
-pygame.init() # initialize any submodules that require it
- 
-# BLUE = pygame.Color("blue") # example, redundant
 WHITE = pygame.Color("white") # example
 
-size = (704, 512) # (width, height) in pixels, example, made size of background picture match (could also do opposite)
-screen = pygame.display.set_mode(size) # set up display
-clock = pygame.time.Clock() # define "clock"
 background_picture = pygame.image.load('images/north_pole.jpeg') # background picture from https://pixy.org/430646/, see License.txt
-background_picture = pygame.transform.scale(background_picture, size)
+background_picture = pygame.transform.scale(background_picture, canvas.size)
 snowflakes = [] # define a list
+y_increment = 1
 i = int() # optional, use range() to confine i to 0 or greater
 r = 4 # circle radius
 
 pygame.display.set_caption("QUESTABOX's Snowfall Animation") # title, example
 
 for i in range(0, 50): # FOR fifty indices (i.e., each index between 0 and, but not including, 50)
-    x = random.randrange(0, size[0]+1) # random number between 0 and, including, size[0]
-    y = random.randrange(0, size[1]+1) # random number between 0 and, including, size[1]
-    snowflakes.append((x, y)) # create a list of random points
+    x = random.randrange(0, canvas.size[0]+1) # random number between 0 and, including, size[0]
+    y = random.randrange(0, canvas.size[1]+1) # random number between 0 and, including, size[1], changed minimum to -size[1] to fill gap that would otherwise develop in animation, reverted
+    snowflakes.append((x, y)) # create a list of random center points
     snowflakes[i] = list(snowflakes[i]) # convert each point to a list (lists within a list), "list" is a class
 
 while True: # keeps display open
     for action in pygame.event.get(): # check for user input when open display
         if action.type == pygame.QUIT: # user clicked close button
-            pygame.quit() # needed if run module through IDLE
-            sys.exit() # exit entire process
+            canvas.close()
     # --- Game logic
     # --------------
-    # screen.fill(BLUE) # clear the display, redundant
+    # canvas.screen.fill(BLUE) # clear the display, redundant
+    canvas.clean() # redundant
     # --- Drawing code
-    screen.blit(background_picture, (0, 0)) # copy the background picture onto the screen starting at (0, 0)
+    canvas.screen.blit(background_picture, (0, 0)) # copy the background picture onto the screen starting at (0, 0)
+    # for center_point in snowflakes: # FOR each item in the list
     for i in range(0, len(snowflakes)): # FOR each index in the list, could also use range(0, 50)
         # pygame.draw.circle(screen, WHITE, snowflakes[i], radius=r, width=1)
-        pygame.draw.circle(screen, WHITE, snowflakes[i], radius=r, width=0)
-        # pygame.draw.circle(screen, WHITE, snowflakes[i], radius=1, width=1)
-        snowflakes[i][1] += 1 # increase y by 1 pixel for each point, if outside loop and relies on mouse, trackpad or keyboard input becomes game logic
-        if snowflakes[i][1] > size[1]+r: # IF snow flake has left the canvas, if outside loop and relies on mouse, trackpad or keyboard input becomes game logic too
+        pygame.draw.circle(canvas.screen, WHITE, snowflakes[i], radius=r, width=0)
+        snowflakes[i][1] += y_increment # increase y by 1 pixel for each point, if outside loop and relies on mouse, trackpad or keyboard input becomes game logic
+        if snowflakes[i][1] > canvas.size[1]+r: # IF snowflake has left the canvas, if outside loop and relies on mouse, trackpad or keyboard input becomes game logic too
             # Recreate it
             # snowflakes[i][1] = random.randrange(0, size[1]+1)
             # Do so above the canvas
             snowflakes[i][1] = random.randrange(-50, -r) # -50 is optional
             # More randomness
-            snowflakes[i][0] = random.randrange(0, size[0]+1)
+            snowflakes[i][0] = random.randrange(0, canvas.size[0]+1)
     # ----------------
-    pygame.display.flip() # update the display
-    clock.tick(60) # maximum 60 frames per second
+    canvas.show()
