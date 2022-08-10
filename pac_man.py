f77bddf899064882313bc660d4b6f41520d9fe0a
Ed Silkworth <ed@questabox.com>
2022-08-09 23:50:33 -0700

    reduce jagged movement

--- pac_man.py
+++ pac_man.py
@@ -219,8 +219,14 @@ while True: # keeps screen open
                 y_increment = 0
                 
         elif action.type == pygame.KEYUP:
-            x_increment = 0
-            y_increment = 0
+            if action.key == pygame.K_RIGHT and x_increment > 0: # being more specific reduces jagged movement
+                x_increment = 0
+            elif action.key == pygame.K_LEFT and x_increment < 0:
+                x_increment = 0
+            elif action.key == pygame.K_UP and y_increment < 0:
+                y_increment = 0
+            elif action.key == pygame.K_DOWN and y_increment > 0:
+                y_increment = 0
             count = 0
             turn(pacman, angle)
 
