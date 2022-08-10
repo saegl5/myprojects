721f5a57ff0e5af4f17a3d5d44407e6b61a19493
Ed Silkworth <ed@questabox.com>
2022-08-09 23:50:12 -0700

    reduce jagged movement

--- space_invaders.py
+++ space_invaders.py
@@ -168,7 +168,9 @@ while True: # keeps screen open
                 x_increment = 0
 
         elif action.type == pygame.KEYUP:
-            if action.key == pygame.K_RIGHT or action.key == pygame.K_LEFT:
+            if action.key == pygame.K_RIGHT and x_increment > 0: # being more specific reduces jagged movement
+                x_increment = 0
+            elif action.key == pygame.K_LEFT and x_increment < 0:
                 x_increment = 0
             elif action.key == pygame.K_SPACE:
                 first = True
