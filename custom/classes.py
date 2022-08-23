073edc9506f71d1f58d4223832de5b3a4f493d2b
Ed Silkworth <ed@questabox.com>
2022-08-23 16:18:03 -0700

    removed line

--- custom/classes.py
+++ custom/classes.py
@@ -11,7 +11,6 @@ class Rectangle(pygame.sprite.Sprite): # Rectangle class of same class as sprite
         super().__init__() # initialize your sprites
         size = (w, h) # size of sprite's image, local variable
         self.image = pygame.Surface(size) # blank image
-        self.image.fill(BLACK) # useful if run module on macOS
         self.image.set_colorkey(BLACK) # removes background, Windows only and newer Python
         self.rect = self.image.get_rect() # pair image with rectangle object
     def update(self, px): # cannot simply name another function/method for group
