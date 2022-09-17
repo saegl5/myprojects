570d822f9a5ca0cf17bff7c0dc2d308121358f24
Ed Silkworth <ed@questabox.com>
2022-09-16 23:12:04 -0700

    aligned dev with main

--- mario.py
+++ mario.py
@@ -99,10 +99,14 @@ while True:
             mario.rect.left = ground.rect.right
     elif hit_platform_x != []:
         for platform in hit_platform_x:
-            if x_inc > 0:
+            if x_inc > 0: # mario moving rightward
                 mario.rect.right = platform.rect.left
+                if halt == True:
+                    x_inc = 0
             else:
                 mario.rect.left = platform.rect.right
+                if halt == True:
+                    x_inc = 0
     if mario.rect.left < ground.rect.left: # could also use mario.rect.x < 0
         mario.rect.left = ground.rect.left
     elif mario.rect.right >= 500: # don't use canvas.SIZE[0] because won't be able to see ahead of you 
@@ -112,7 +116,7 @@ while True:
             platform.rect.x -= move_left
         mario.rect.right = 500 # keep mario still, also maintains correct change in movement
     elif mario.rect.left <= 200:
-        if ground.rect.x < 0: # helps preserve boundary
+        if ground.rect.x < 0: # helps preserve boundary <-- use wall?????
             move_right = 200 - mario.rect.left
             if ground.rect.x + move_right > 0: # securely preserves boundary
                 over = ground.rect.x + move_right # negative + positive
@@ -137,8 +141,8 @@ while True:
     elif hit_platform_y != []:
         for platform in hit_platform_y:
             if y_inc < 0: # in jump
-                mario.rect.top = platform.rect.bottom # bonks his head on platform
-                y_inc = 0 # unsticks mario
+                mario.rect.top = platform.rect.bottom # hits his head
+                y_inc = 0 # unsticks mario, otherwise y_inc won't change
             else: # falling or plateaued
                 mario.rect.bottom = platform.rect.top
                 y_inc = 0 # in case mario walks off platform
