c2e15392cb0fd7267bb9ab87ac131855951ee283
Ed Silkworth <ed@questabox.com>
2023-04-22 00:05:48 -0700

    align dev with main

--- mario.py
+++ mario.py
@@ -79,11 +79,17 @@ mario.image.blit(mario_frames, (0, 0), (frame1[0][0], frame1[0][1], W_mario, H_m
 frame2 = [  (0,          0,          W_goomba, H_goomba),
             (W_goomba,   0,          W_goomba, H_goomba),
             (2*W_goomba, H_goomba/2, W_goomba, H_goomba/2)  ]
-clones = [  (600,                 canvas.SIZE[1]-GH-H_goomba, frame2[0][2], frame2[0][3]),
-            (canvas.SIZE[0]+1100, canvas.SIZE[1]-GH-H_goomba, frame2[0][2], frame2[0][3]),
-            (canvas.SIZE[0]+2975, canvas.SIZE[1]-GH-H_goomba, frame2[0][2], frame2[0][3])  ]
-            # each goomba sprite starts out near end of ground sprite
-            # xN = xN-1 + wN-1
+clones = [  (600,                 canvas.SIZE[1]-GH-H_goomba, W_goomba, H_goomba),
+            (canvas.SIZE[0]+1100, canvas.SIZE[1]-GH-H_goomba, W_goomba, H_goomba),
+            (canvas.SIZE[0]+2975, canvas.SIZE[1]-GH-H_goomba, W_goomba, H_goomba)  ]
+            # keep y, width and height, only change x
+            # first goomba sprite starts out near end of first ground sprite            
+            
+            # canvas.SIZE[0]+1100 = canvas.SIZE[0]+500 + 600
+            # second goomba sprite will start out to right edge of third ground sprite
+            
+            # canvas.SIZE[0]+2975 = canvas.SIZE[0]+2275 + 700
+            # third goomba sprite will start out to right edge of last ground sprite
 goombas = pygame.sprite.Group()
 for clone in clones: # each clone
     goomba = Rectangle(clone[2], clone[3])
