9f96191d9d5940c2d66128d7579f8053601142b2
Ed Silkworth <ed@questabox.com>
2023-05-27 00:15:47 -0700

    updated comment

--- mario.py
+++ mario.py
@@ -59,7 +59,7 @@ for block in blocks1: # each block
     ground.rect.x = block[0]
     ground.rect.y = block[1]
     # fill each rectangle from left to right
-    for i in range(0, block[2], round(70*GH/105)): # again block[2] is rectangle width, 70 pixels is step size based on width of each image
+    for i in range(0, block[2], round(70*GH/105)): # again block[2] is rectangle width, round(70*GH/105) pixels is step size based on width of each image
         ground.image.blit(ground_middle, (i, 0))
     grounds.add(ground)
 
