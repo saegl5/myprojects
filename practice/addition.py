09a994dd4280e9091570733a3a7b32fc01b86474
Ed Silkworth <ed@questabox.com>
2023-07-05 23:07:55 +0800

    added alternative solution

--- practice/addition.py
+++ practice/addition.py
@@ -6,3 +6,15 @@ def addition(a, b):
 print(addition(3, 2)) # 5
 print(addition(-3, -6)) # -9
 print(addition(7, 3)) # 10
+
+# alternative
+
+a = 3
+b = 2
+
+def addition(a, b): # could also use different variables
+    return a + b
+
+print(addition(a, b)) # 5
+# repeat for a = -3 and b = -6 # -9
+# repeat for a = 7 and b = 3 # 10
