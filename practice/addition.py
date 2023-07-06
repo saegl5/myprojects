747ecf542bc1cd6bfc18f99851bdc236e1c9109d
Ed Silkworth <ed@questabox.com>
2023-07-06 22:12:05 +0800

    redoing

--- practice/addition.py
+++ practice/addition.py
@@ -1,5 +1,16 @@
 # source: https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
 
+# simplest
+
+def addition(a, b):
+    print(a + b)
+
+addition(3, 2) # 5
+addition(-3, -6) # -9
+addition(7, 3) # 10
+
+# alternative using `return`
+
 def addition(a, b):
     return a + b
 
@@ -7,14 +18,14 @@ print(addition(3, 2)) # 5
 print(addition(-3, -6)) # -9
 print(addition(7, 3)) # 10
 
-# alternative
+# alternative using `global`
 
-a = 3
-b = 2
+s = int()
 
-def addition(a, b): # could also use different variables
-    return a + b
+def addition(a, b):
+    global s
+    s = a + b
 
-print(addition(a, b)) # 5
-# repeat for a = -3 and b = -6 # -9
-# repeat for a = 7 and b = 3 # 10
+addition(3, 2); print(s) # 5
+addition(-3, -6); print(s) # -9
+addition(7, 3); print(s) # 10
--- practice/subtraction.py
+++ practice/subtraction.py
@@ -1,8 +1,8 @@
 # source: questabox
 
 def subtraction(a, b):
-    return abs(a - b)
+    print(abs(a - b))
 
-print(subtraction(7, 3)) # 4
-print(subtraction(2, 3)) # 1
-print(subtraction(-3, -6)) # 3
+subtraction(7, 3) # 4
+subtraction(2, 3) # 1
+subtraction(-3, -6) # 3
