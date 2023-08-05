3470e47d0909ab978014dc3ac4bb3e06566a7468
Ed Silkworth <ed@questabox.com>
2023-08-04 22:50:44 -0700

    aligned dev with main

--- practice/subtraction.py
+++ practice/subtraction.py
@@ -1,5 +1,16 @@
 # source: questabox
 
+# less
+
+def subtraction(a, b):
+    print(a - b)
+
+subtraction(7, 3) # 4
+subtraction(2, 3) # -1
+subtraction(-3, -6) # 3
+
+# difference
+
 def subtraction(a, b):
     print(abs(a - b))
 
