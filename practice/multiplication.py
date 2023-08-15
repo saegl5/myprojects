29d3f46be702bd50e17159e0bf2449b3448fc0cf
Ed Silkworth <ed@questabox.com>
2023-08-15 14:11:04 -0700

    added examples

--- practice/multiplication.py
+++ practice/multiplication.py
@@ -1,9 +1,21 @@
 # source: questabox
 
+# product
+
 def multiplication(a, b):
-    print(a*b)
+    print(a * b)
 
 multiplication(3, 8) # 24
 multiplication(0, 10) # 0
 multiplication(-6, 5) # -30
 multiplication(-6, -5) # 30
+
+# power
+
+def multiplication(a, b):
+    print(a ** b)
+
+multiplication(1, 3) # 1
+multiplication(2, 3) # 8
+multiplication(-2, 3) # -8
+multiplication(3, 0) # 1
