3365441bcca36f200d386ec75ad9eef5d9cf4197
Ed Silkworth <ed@questabox.com>
2023-08-18 23:43:00 -0700

    aligned dev with main

---
+++ practice/division.py
@@ -0,0 +1,21 @@
+# source: questabox
+
+# quotient
+
+def division(a, b):
+    print(a / b)
+
+division(5, 1) # 5.0
+division(5, 2) # 2.5
+division(-6, 2) # -3.0
+division(-6, -4) # 1.5
+
+# remainder
+
+def division(a, b):
+    print(a % b)
+
+division(5, 1) # 0
+division(5, 2) # 1
+division(-6, 2) # 0
+division(-6, -4) # -2
