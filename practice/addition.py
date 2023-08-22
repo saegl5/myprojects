3f5e956cb47cc7eaf03178d4091df71920bf84da
Ed Silkworth <ed@questabox.com>
2023-08-21 22:21:05 -0700

    align with edabit style

--- practice/addition.py
+++ practice/addition.py
@@ -1,8 +1,8 @@
 # source: https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
 
 def addition(a, b):
-    print(a + b)
+    return a + b
 
-addition(3, 2) # 5
-addition(-3, -6) # -9
-addition(7, 3) # 10
+print(addition(3, 2)) # 5
+print(addition(-3, -6)) # -9
+print(addition(7, 3)) # 10
