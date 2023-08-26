3a634944520b998cdfca78f1b2905fe5451be38b
Ed Silkworth <ed@questabox.com>
2023-08-25 22:02:38 -0700

    added comment

--- practice/addition.py
+++ practice/addition.py
@@ -1,7 +1,7 @@
 # source: https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
 
 def addition(a, b):
-    return a + b
+    return a + b # alternative: sum([a, b])
 
 print(addition(3, 2)) # 5
 print(addition(-3, -6)) # -9
