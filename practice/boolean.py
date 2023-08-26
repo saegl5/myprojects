16bcb7bc27700a751ae5de26d6373687ebbbd2ce
Ed Silkworth <ed@questabox.com>
2023-08-25 21:59:35 -0700

    added practice problem

---
+++ practice/boolean.py
@@ -0,0 +1,11 @@
+# source: https://edabit.com/challenge/pZ3HxBfvejsvkEDo4
+
+def less_than_100(a, b):
+    if a + b < 100: # alternative: sum([a, b]) < 100
+        return True
+    else:
+        return False
+
+print(less_than_100(22, 15)) # True
+print(less_than_100(83, 34)) # False
+print(less_than_100(3, 77)) # True
