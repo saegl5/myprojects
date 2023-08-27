6f18806a68f47fdf8ffe8d6bca33fff0cdb68696
Ed Silkworth <ed@questabox.com>
2023-08-26 22:35:27 -0700

    align problems with edabit better

---
+++ practice/absolute.py
@@ -0,0 +1,11 @@
+# source: https://edabit.com/challenge/DbFcBJiRz3GDm6xns
+
+def absolute(n): # recreated: abs(n)
+	if n < 0:
+		return -1*n
+	else:
+		return n
+
+print(absolute(-5)) # 5
+print(absolute(-3.14)) # 3.14
+print(absolute(250)) # 250
---
+++ practice/remainder.py
@@ -0,0 +1,9 @@
+# source: https://edabit.com/challenge/KWoj7kWiHRqJtG6S2
+
+def remainder(x, y):
+    return x % y # quotient: a / b
+
+print(remainder(1, 3)) # 1
+print(remainder(3, 4)) # 3
+print(remainder(5, 5)) # 0
+print(remainder(7, 2)) # 1
