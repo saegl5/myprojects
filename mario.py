b932983d40628afa35c3aabdba13533caca5f52d
Ed Silkworth <ed@questabox.com>
2023-09-15 22:08:14 -0700

    realigned lines

--- mario.py
+++ mario.py
@@ -85,9 +85,9 @@ for block in blocks1: # each block
     ground.image.blit(ground_right, (block[2]-W_scaled_ground, 0))
     grounds.add(ground)
 
-frame1 = [  (9,         7,         W_mario-17, H_mario-13), 
-            (1,          H_mario+9, W_mario-7,  H_mario-12),
-            (W_mario+6, H_mario+8, W_mario-16, H_mario-10),
+frame1 = [  (9,          7,          W_mario-17, H_mario-13), 
+            (1,          H_mario+9,  W_mario-7,  H_mario-12),
+            (W_mario+6,  H_mario+8,  W_mario-16, H_mario-10),
             (W_mario+3,  3,          W_mario-8,  H_mario-9)  ]
 # first mario frame is for standing still, second and third for walking, and fourth for jumping
 # will not loop frame list, so to call any parameter use two indices
